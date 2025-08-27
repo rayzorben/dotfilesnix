ef1a0ea3f458329c0dd125b7be3b23a7cdc2a667
Raymond Bennett <raymond.bennett@gmail.com>
2025-08-26 22:56:00 -0700

    added password

--- modules/users.nix
+++ modules/users.nix
@@ -6,6 +6,7 @@
     description = "Ray Bennett";
     extraGroups = [ "networkmanager" "wheel" "libvirt" ];
     shell = pkgs.fish;
+    hashedPassword = "$6$.7VXwaA6$bt9jvRb8VxYMSQCtdyoyGyFhXGkAdTPDW/a1RCT8DPQzLYCGtze6L3jGVpMSatsJ21PoZrToFpQ6JqDLR3mjR1";
   };
 
   security.sudo.extraRules = [
